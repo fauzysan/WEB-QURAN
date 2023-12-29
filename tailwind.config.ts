b8de644a946ba165ea597181176c8927d73d4eb1
fauzysan <fauzygaming47@gmail.com>
2023-12-29 20:29:05 +0700

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        while: '#ffffff',
+        black: '#092635',
+        teal: '#1B4242',
+        sage: '#5C8374',
+        winter: '#9EC8B9'
+      },
+      fontFamily: {
+        Poppins: "Poppins",
+        Serif: "Serif"
+      }
     },
   },
   plugins: [],
